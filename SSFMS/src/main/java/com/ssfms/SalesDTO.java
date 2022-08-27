ef8f2d181fce74228c63b6029e415c624c51bc39
ssj04 <ssj04@DESKTOP-M70FNUN>
2022-08-27 21:12:18 +0900

    2022-08-27 수정필요

--- SSFMS/src/main/java/com/ssfms/SalesDTO.java
+++ SSFMS/src/main/java/com/ssfms/SalesDTO.java
@@ -9,6 +9,49 @@ public class SalesDTO {
 	private String customer;
 	private String sales;
 	private String salesQty;
+	
+	public String getSalesNo() {
+		return salesNo;
+	}
+	public void setSalesNo(String salesNo) {
+		this.salesNo = salesNo;
+	}
+	public String getStateNo() {
+		return stateNo;
+	}
+	public void setStateNo(String stateNo) {
+		this.stateNo = stateNo;
+	}
+	public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	public String getSales() {
+		return sales;
+	}
+	public void setSales(String sales) {
+		this.sales = sales;
+	}
+	public String getSalesQty() {
+		return salesQty;
+	}
+	public void setSalesQty(String salesQty) {
+		this.salesQty = salesQty;
+	}
+	public String getDeal_Date() {
+		return deal_Date;
+	}
+	public void setDeal_Date(String deal_Date) {
+		this.deal_Date = deal_Date;
+	}
 	private String deal_Date;
 	
 	

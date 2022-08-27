5e0ffa833d24e30da43cf552caff41765dcf9ce8
song-yujin <song-yujin@localhost>
2022-08-27 21:51:53 +0900

    Merge branch 'main' of https://github.com/Yeongwookang/SSFMS.git into main

--- SSFMS/src/main/java/com/ssfms/AccDAOImpl.java
+++ SSFMS/src/main/java/com/ssfms/AccDAOImpl.java
@@ -2,12 +2,15 @@ package com.ssfms;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.util.DBConn;
+
+import oracle.jdbc.OracleTypes;
 
 public class AccDAOImpl implements AccDAO {
 	private Connection conn = DBConn.getConnection();
@@ -64,44 +67,351 @@ public class AccDAOImpl implements AccDAO {
 
 	@Override
 	public int updateAccount(AccDTO dto) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		CallableStatement cstmt = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL updateAccount(?,?,?,?,?,?,?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.setString(1, dto.getEmpNo());
+			cstmt.setString(2, dto.getAccountNo());
+			cstmt.setString(3, dto.getAccountSubNo());
+			cstmt.setString(4, dto.getAmount());
+			cstmt.setString(5, dto.getDetail());
+			cstmt.setString(6, dto.getCancellation());
+			cstmt.setString(7, dto.getStateCon());
+			
+			cstmt.executeUpdate();
+			
+			result = 1;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
-	public int deleteAccount(AccDTO stateNum) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteAccount(AccDTO stateNo) throws SQLException {
+		int result = 0;
+		CallableStatement cstmt = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL deleteAccount(?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.setString(1, stateNo);
+			
+			cstmt.executeUpdate();
+			
+			result = 1;
+			
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 20100) {
+				System.out.println("등록된 자료가 아닙니다.");
+			} else {
+				e.printStackTrace();
+			}
+			
+			throw e;
+			
+		} finally {
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
 	public AccDTO readAccount(String stateNum) {
-		// TODO Auto-generated method stub
-		return null;
+		AccDTO dto = null;
+		CallableStatement cstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL readAccount(?,?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			cstmt.setString(2, stateNum);
+			
+			cstmt.executeUpdate();
+			
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			if(rs.next()) {
+				dto = new AccDTO();
+
+				dto.setStateNum(rs.getString("stateNum"));
+				dto.setEmpNo(rs.getString("empNo"));
+				dto.setAccountNo(rs.getString("accountNo"));
+				dto.setAccountSubNo(rs.getString("accountSubNo"));
+				dto.setAmount(rs.getString("amount"));
+				dto.setDetail(rs.getString("detail"));
+				dto.setCancellation(rs.getString("cancellation"));
+				dto.setStateCon(rs.getString("stateCon"));
+				dto.setStateDate(rs.getString("stateDate"));	
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return dto;
 	}
 
 	@Override
 	public List<AccDTO> listAccount() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		List<AccDTO> list = new ArrayList<>();
+		CallableStatement cstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL listAccount(?)}";
+			
+cstmt = conn.prepareCall(sql);
+			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			
+			cstmt.executeUpdate();
+			
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			while(rs.next()) {
+				AccDTO dto = new AccDTO();
 
+				dto.setStateNum(rs.getString("stateNum"));
+				dto.setEmpNo(rs.getString("empNo"));
+				dto.setAccountNo(rs.getString("accountNo"));
+				dto.setAccountSubNo(rs.getString("accountSubNo"));
+				dto.setAmount(rs.getString("amount"));
+				dto.setDetail(rs.getString("detail"));
+				dto.setCancellation(rs.getString("cancellation"));
+				dto.setStateCon(rs.getString("stateCon"));
+				dto.setStateDate(rs.getString("stateDate"));
+				
+				list.add(dto);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+		}
+		
+		return list;
+	}
 	@Override
 	public List<AccDTO> listAccount_emp(String empNo) {
-		// TODO Auto-generated method stub
-		return null;
+		List<AccDTO> list = new ArrayList<>();
+		CallableStatement cstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL searchempNoAcc(?,?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			cstmt.setString(2, empNo);
+			
+			cstmt.executeUpdate();
+			
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			while(rs.next()) {
+				AccDTO dto = new AccDTO();
+
+				dto.setStateNum(rs.getString("stateNum"));
+				dto.setEmpNo(rs.getString("empNo"));
+				dto.setAccountNo(rs.getString("accountNo"));
+				dto.setAccountSubNo(rs.getString("accountSubNo"));
+				dto.setAmount(rs.getString("amount"));
+				dto.setDetail(rs.getString("detail"));
+				dto.setCancellation(rs.getString("cancellation"));
+				dto.setStateCon(rs.getString("stateCon"));
+				dto.setStateDate(rs.getString("stateDate"));
+				
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+		}
+		
+		return list;
 	}
 
 	@Override
 	public List<AccDTO> listAccount_categ(String categName) {
-		// TODO Auto-generated method stub
-		return null;
+		List<AccDTO> list = new ArrayList<>();
+		CallableStatement cstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL searchcategNameAcc(?,?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			cstmt.setString(2, categName);
+			
+			cstmt.executeUpdate();
+			
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			while(rs.next()) {
+				AccDTO dto = new AccDTO();
+
+				dto.setStateNum(rs.getString("stateNum"));
+				dto.setEmpNo(rs.getString("empNo"));
+				dto.setAccountNo(rs.getString("accountNo"));
+				dto.setAccountSubNo(rs.getString("accountSubNo"));
+				dto.setAmount(rs.getString("amount"));
+				dto.setDetail(rs.getString("detail"));
+				dto.setCancellation(rs.getString("cancellation"));
+				dto.setStateCon(rs.getString("stateCon"));
+				dto.setStateDate(rs.getString("stateDate"));
+				
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+		}
+		
+		return list;
 	}
 
 	@Override
 	public List<AccDTO> listAccount_subNo(String accountSubNo) {
-		// TODO Auto-generated method stub
-		return null;
+		List<AccDTO> list = new ArrayList<>();
+		CallableStatement cstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "{ CALL searchcategNameAcc(?,?) }";
+			cstmt = conn.prepareCall(sql);
+			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
+			cstmt.setString(2, accountSubNo);
+			
+			cstmt.executeUpdate();
+			
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			while(rs.next()) {
+				AccDTO dto = new AccDTO();
+
+				dto.setStateNum(rs.getString("stateNum"));
+				dto.setEmpNo(rs.getString("empNo"));
+				dto.setAccountNo(rs.getString("accountNo"));
+				dto.setAccountSubNo(rs.getString("accountSubNo"));
+				dto.setAmount(rs.getString("amount"));
+				dto.setDetail(rs.getString("detail"));
+				dto.setCancellation(rs.getString("cancellation"));
+				dto.setStateCon(rs.getString("stateCon"));
+				dto.setStateDate(rs.getString("stateDate"));
+				
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+
+			if(cstmt != null) {
+				try {
+					cstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+			
+		}
+		
+		return list;
 	}
 
 }
--- SSFMS/src/main/java/com/ssfms/AccUI.java
+++ SSFMS/src/main/java/com/ssfms/AccUI.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 
 import com.util.DBConn;
 
+
+
 public class AccUI {
 	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private AccDAO dao = new AccDAOImpl();
@@ -79,14 +81,59 @@ public class AccUI {
 	
 	protected void update() {
 		System.out.println("\n전표 수정 ");
-	
+		
+		try {
+			AccDTO dto = new AccDTO();
+			
+			System.out.print("수정할 사원코드");
+			dto.setEmpNo(br.readLine());
+			
+			System.out.print("수정할 계좌코드");
+			dto.setAccountNo(br.readLine());
+			
+			System.out.print("수정할 계정과목코드");
+			dto.setAccountSubNo(br.readLine());
+			
+			System.out.print("수정할 금액");
+			dto.setAmount(br.readLine());
+			
+			System.out.print("수정할 상세내용");
+			dto.setDetail(br.readLine());
+			
+			System.out.print("수정할 취소여부");
+			dto.setCancellation(br.readLine());
+			
+			System.out.print("수정할 전표상태");
+			dto.setStateCon(br.readLine());
+			
+			dao.updateAccount(dto);
+			
+			System.out.println("전표가 수정 되었습니다.");
+			
+		} catch (Exception e) {
+			System.out.println("수정 실패");
+		}
 	}
 	
 	protected void delete() {
 		System.out.println("\n전표 삭제 ");
-	
+		String stateNo;
+		
+		try {
+			System.out.print("삭제할 전표 번호 ? ");
+			stateNo = br.readLine();
+			
+			dao.deleteAccount(stateNo);
+			
+			System.out.println("전표가 삭제 되었습니다.");
+		} catch (Exception e) {
+			System.out.println("전표 삭제 실패 !!!");
+		}
+		System.out.println();
 		
 	}
+		
+	
 	
 	protected void listAll() {
 		System.out.println("\n전체 전표 조회 ");
@@ -95,16 +142,61 @@ public class AccUI {
 	
 	protected void findByempNo() {
 		System.out.println("\n사원별 전표 조회 ");
-
+		String empNo;
 		
+		try {
+			System.out.print("검색할 사원 ? ");
+			empNo = br.readLine();
+			
+			AccDTO dto = dao.readAccount(empNo);
+			if(dto == null) {
+				System.out.println("등록된 사원이 아닙니다.\n");
+				return;
+			}
+			
+			System.out.print(dto.getStateNum()+"\t");
+			System.out.print(dto.getEmpNo()+"\t");
+			System.out.print(dto.getAccountNo()+"\t");
+			System.out.print(dto.getAccountSubNo()+"\t");
+			System.out.print(dto.getAmount()+"\t");
+			System.out.print(dto.getDetail()+"\t");
+			System.out.print(dto.getCancellation()+"\t");
+			System.out.print(dto.getStateCon()+"\t");
+			System.out.println(dto.getStateDate());
+				
+		} catch (Exception e) {
+		}
+			System.out.println();
 	}
 	
 	protected void findByname() {
-		System.out.println("\n계정과목 별 전표 조회 ");
-
+		System.out.println("\n계정과목명 별 전표 조회 ");
+		String categName;
 		
+		try {
+			System.out.print("계정과목명을 입력하세요. ");
+			categName = br.readLine();
+			
+			AccDTO dto = dao.readAccount(categName);
+			if(dto == null) {
+				System.out.println("해당 계정과목으로 등론된 전표가 존재하지 않습니다.\n");
+				return;
+			}
+			
+			System.out.print(dto.getStateNum()+"\t");
+			System.out.print(dto.getEmpNo()+"\t");
+			System.out.print(dto.getAccountNo()+"\t");
+			System.out.print(dto.getAccountSubNo()+"\t");
+			System.out.print(dto.getAmount()+"\t");
+			System.out.print(dto.getDetail()+"\t");
+			System.out.print(dto.getCancellation()+"\t");
+			System.out.print(dto.getStateCon()+"\t");
+			System.out.println(dto.getStateDate());
+				
+		} catch (Exception e) {
+		}
+			System.out.println();
 	}
-	
 	protected void findBycateg_Name() {
 		System.out.println("\n월별 총 매입/매출액 ");
 

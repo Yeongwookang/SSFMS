e0e799206260264974d3312d05f5979acfb26676
ssj04 <ssj04@DESKTOP-M70FNUN>
2022-08-29 22:59:40 +0900

    2022-08-29 nn

--- SSFMS/src/main/java/com/ssfms/SalesDAOImpl.java
+++ SSFMS/src/main/java/com/ssfms/SalesDAOImpl.java
@@ -1,14 +1,14 @@
 package com.ssfms;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLDataException;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 import com.util.DBConn;
 
-
-
-//임시 변경사항있음
 
 public class SalesDAOImpl implements SalesDAO {
 	private Connection conn = DBConn.getConnection();
@@ -18,21 +18,9 @@ public class SalesDAOImpl implements SalesDAO {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-	@Override
-	public int orderInsertSales(SalesDTO dto) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 	@Override
 	public int updateSales(SalesDTO dto) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int deleteSales(String id) throws SQLException {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -55,7 +43,135 @@ public class SalesDAOImpl implements SalesDAO {
 		return null;
 	}
 
+	@Override
+	public int salesInsert(SalesDTO dto) throws SQLException {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql;
 
+		try {
+			sql = "INSERT INTO salesInsert (salesNo, stateNo, productCode, customer, sales, salesQty, dealDate)"
+					+ " VALUES (?, ?, ?, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getSalesNo());
+			pstmt.setInt(2, dto.getStateNo());
+			pstmt.setString(3, dto.getProductCode());
+			pstmt.setString(4, dto.getCustomer());
+			pstmt.setInt(5, dto.getSales());
+			pstmt.setInt(6, dto.getSalesQty());
+			pstmt.setString(7, dto.getDealDate());	
+
+		} catch (SQLIntegrityConstraintViolationException e) {
+
+			if (e.getErrorCode() == 1) {
+				System.out.println("중복 데이터 등록은 불가합니다.");
+			} else if (e.getErrorCode() == 1400) {
+				System.out.println("필수 사항을 입력하지 않았습니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+
+		} catch (SQLDataException e) {
+			if (e.getErrorCode() == 1840 || e.getErrorCode() == 1861) {
+				System.out.println("날짜 입력 형식 오류입니다.");
+				System.out.println("예시) 2001-01-01");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public int salesAccountInsert(SalesDTO dto) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
-}
 
+	@Override
+	public int taxBillInsert(SalesDTO dto) throws SQLException {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql;
+
+		try {
+			sql = "INSERT INTO taxBill(taxBillNum, salesNo, companyName, name, address, busStatue, currDate, valueSupply, taxAmount,"
+					+ "item, num, unitPrice, total, outAmount, note)"
+					+ " VALUES(?, sales_no_seq.nextval, ?, ?, ?, ?, SYSDATE, ?, ?, ?, ?, ?, ?, ?, ?)";
+			
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, dto.getTaxBillNum());
+			pstmt.setString(2, dto.getCompanyName());
+			pstmt.setString(3, dto.getName());
+			pstmt.setString(4, dto.getAddress());
+			pstmt.setString(5, dto.getBusStatue());
+			pstmt.setInt(6, dto.getValueSupply());
+			pstmt.setInt(7, dto.getTaxAmount());
+			pstmt.setString(8, dto.getItem());
+			pstmt.setInt(9, dto.getNum());
+			pstmt.setInt(10, dto.getUnitPrice());
+			pstmt.setInt(11, dto.getTotal());
+			pstmt.setInt(12, dto.getOutAmount());
+			pstmt.setString(13, dto.getNote());
+
+		} catch (SQLIntegrityConstraintViolationException e) {
+
+			if (e.getErrorCode() == 1) {
+				System.out.println("중복 데이터 등록은 불가합니다.");
+			} else if (e.getErrorCode() == 1400) {
+				System.out.println("필수 사항을 입력하지 않았습니다.");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+
+		} catch (SQLDataException e) {
+			if (e.getErrorCode() == 1840 || e.getErrorCode() == 1861) {
+				System.out.println("날짜 입력 형식 오류입니다.");
+				System.out.println("예시) 2001-01-01");
+			} else {
+				System.out.println(e.toString());
+			}
+			throw e;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+
+}
--- SSFMS/src/main/java/com/ssfms/SalesDTO.java
+++ SSFMS/src/main/java/com/ssfms/SalesDTO.java
@@ -3,12 +3,30 @@ package com.ssfms;
 
 //임시 변경사항있음
 public class SalesDTO {
+	//매출
 	private String salesNo;
-	private String stateNo;
+	private int stateNo;
 	private String productCode;
 	private String customer;
-	private String sales;
-	private String salesQty;
+	private int sales;
+	private int salesQty;
+	private String dealDate;
+	
+	//세금계산서
+	private int taxBillNum;
+	private String companyName;
+	private String name;
+	private String address;
+	private String busStatue;
+	private int valueSupply;
+	private int taxAmount;
+	private String item;
+    private int num;
+    private int unitPrice;
+    private int total;
+    private int outAmount;
+    private String note;
+	
 	
 	public String getSalesNo() {
 		return salesNo;
@@ -16,10 +34,10 @@ public class SalesDTO {
 	public void setSalesNo(String salesNo) {
 		this.salesNo = salesNo;
 	}
-	public String getStateNo() {
+	public int getStateNo() {
 		return stateNo;
 	}
-	public void setStateNo(String stateNo) {
+	public void setStateNo(int stateNo) {
 		this.stateNo = stateNo;
 	}
 	public String getProductCode() {
@@ -34,25 +52,102 @@ public class SalesDTO {
 	public void setCustomer(String customer) {
 		this.customer = customer;
 	}
-	public String getSales() {
+	public int getSales() {
 		return sales;
 	}
-	public void setSales(String sales) {
+	public void setSales(int sales) {
 		this.sales = sales;
 	}
-	public String getSalesQty() {
+	public int getSalesQty() {
 		return salesQty;
 	}
-	public void setSalesQty(String salesQty) {
+	public void setSalesQty(int salesQty) {
 		this.salesQty = salesQty;
 	}
-	public String getDeal_Date() {
-		return deal_Date;
+	public int getTaxBillNum() {
+		return taxBillNum;
 	}
-	public void setDeal_Date(String deal_Date) {
-		this.deal_Date = deal_Date;
+	public void setTaxBillNum(int taxBillNum) {
+		this.taxBillNum = taxBillNum;
 	}
-	private String deal_Date;
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBusStatue() {
+		return busStatue;
+	}
+	public void setBusStatue(String busStatue) {
+		this.busStatue = busStatue;
+	}
+	public int getValueSupply() {
+		return valueSupply;
+	}
+	public void setValueSupply(int valueSupply) {
+		this.valueSupply = valueSupply;
+	}
+	public int getTaxAmount() {
+		return taxAmount;
+	}
+	public void setTaxAmount(int taxAmount) {
+		this.taxAmount = taxAmount;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getOutAmount() {
+		return outAmount;
+	}
+	public void setOutAmount(int outAmount) {
+		this.outAmount = outAmount;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getDealDate() {
+		return dealDate;
+	}
+	public void setDealDate(String dealDate) {
+		this.dealDate = dealDate;
+	}
 	
 	
 	
